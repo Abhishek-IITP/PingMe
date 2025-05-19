dcff2444c1b9cceef04d5fc6ecfdd17fad791c4c
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-05-19 15:50:41 +0530

    create

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-import path from "path"
 
 import authRoutes from "./Routes/authRoute.js"
 import userRoutes from "./Routes/userRoutes.js"
@@ -14,23 +13,13 @@ import { connectDB } from "./lib/dbConnect.js";
 const app = express()
 const Port =process.env.PORT
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({    
     origin: "http://localhost:5173",
     credentials: true,
 }))
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
-  });
-}
-
-
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
